41e3bb57545ddff92d76ff9fe2c5cd8b2d90bada
palomartve <paloma.guiral@rtve.es>
2025-03-11 13:12:06 +0100

    comentarios

--- app/crud.py
+++ app/crud.py
@@ -8,11 +8,11 @@ Contiene funciones para interactuar con la base de datos.
 """
 
 from sqlalchemy.orm import Session
-from .models import ChatMessage
+from .models import ChatMessage #Es el modelo de base de datos definido en models.py (representa la tabla chat_messages).
 
 def save_chat(db: Session, user_message: str, bot_response: str):
-    chat = ChatMessage(user_message=user_message, bot_response=bot_response)
-    db.add(chat)
-    db.commit()
-    db.refresh(chat)
-    return chat
+    chat = ChatMessage(user_message=user_message, bot_response=bot_response) #Crea un nuevo objeto ChatMessage con los datos.
+    db.add(chat) #Lo agrega a la sesión con db.add(chat)
+    db.commit() #Confirma los cambios en la base de datos con db.commit().
+    db.refresh(chat) #Recarga el objeto desde la base de datos con db.refresh(chat).
+    return chat #Devuelve el objeto chat guardado.
